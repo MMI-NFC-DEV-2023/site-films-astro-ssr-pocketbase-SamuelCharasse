b098a280bebd2fcf534dcd238023c446d836e383
Hasmed <samuel.charasse@edu.univ-fcomte.fr>
2025-02-17 14:18:51 +0100

    ajout de l'adaptateur Node et mise à jour des dépendances, réorganisation du composant Debug dans la page des personnes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,17 @@ import alpinejs from '@astrojs/alpinejs';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [alpinejs()],
 
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: node({
+    mode: 'standalone'
+  })
 });
